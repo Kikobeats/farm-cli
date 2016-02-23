f7174eb205b4cbc55389b8ca8c8e08b483fe0fa7
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-23 10:57:10 +0100

    Extend support to 0.10

--- bin/config.js
+++ bin/config.js
@@ -3,20 +3,20 @@
 var fs = require('fs')
 var path = require('path')
 var existFile = require('exists-file')
-
-var minimistPath = path.join(__dirname, '..', 'node_modules', 'meow', 'node_modules', 'minimist')
-var minimist = require(minimistPath)
-
 var trimNewlinesPath = path.join(__dirname, '..', 'node_modules', 'meow', 'node_modules', 'trim-newlines')
 var trimNewlines = require(trimNewlinesPath)
+var isDirectory = require('is-directory').sync
 
 var FILENAME = 'worker-farm.opts'
 
-function readConfig (filepath) {
+function readConfig () {
   var yargs = process.argv.slice(2)
   if (yargs.length !== 1) return yargs
 
-  var filepath = path.resolve(yargs[0], FILENAME)
+  var filepath = path.resolve(yargs[0])
+  if (!isDirectory(filepath)) filepath = path.join(filepath, '..')
+  filepath = path.join(filepath, FILENAME)
+
   if (!existFile(filepath)) return yargs
 
   var config = fs.readFileSync(filepath, 'utf8')
