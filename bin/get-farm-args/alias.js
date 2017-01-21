c31b4881f41dd0527f778a20ae7ac9a3a5481155
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-21 23:32:07 +0100

    Improve alias

--- bin/get-farm-args/alias.js
+++ bin/get-farm-args/alias.js
@@ -2,8 +2,12 @@
 
 module.exports = {
   d: 'delayBetweenWorkers',
+  delay: 'delayBetweenWorkers',
   n: 'maxConcurrentWorkers',
+  cores: 'maxConcurrentWorkers',
   w: 'maxConcurrentCallsPerWorker',
+  workers: 'maxConcurrentCallsPerWorker',
   r: 'maxRetries',
+  retries: 'maxRetries',
   autostart: 'autoStart'
 }
