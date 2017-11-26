53e64d2108959b960a09274ebf2cc4ec8a36bf7f
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-11-26 19:28:36 +0100

    Improve fibonacci example

--- examples/fibonacci/index.js
+++ examples/fibonacci/index.js
@@ -5,29 +5,33 @@ const mutexify = require('mutexify')
 
 const memo = [0, 1]
 
-const fibonacci = function (n) {
+const fibonacciMemo = function (n) {
   var result = memo[n]
   if (typeof result === 'number') return result
-  result = fibonacci(n - 1) + fibonacci(n - 2)
+  result = fibonacciMemo(n - 1) + fibonacciMemo(n - 2)
   memo[n] = result
   return result
 }
+
+const fibonacci = num => num <= 1 ? 1 : fibonacci(num - 1) + fibonacci(num - 2)
 
 const lock = mutexify()
 let index = 0
 
 module.exports = function (opts, cb) {
-  const {worker: workerNum} = opts
+  const {worker: workerNum, memo, n} = opts
+  const fn = memo ? fibonacciMemo : fibonacci
 
   whilst(
-    () => true,
+    () => index < n,
     done => waterfall([
       next => lock(release => release(next(null, ++index))),
       ensureAsync((num, next) => {
-        const value = fibonacci(num)
+        const value = fn(num)
         console.log(`#${workerNum} fibonacci value=${index} result=${value}`)
         return next()
       })
-    ], done)
+    ], done),
+    cb
   )
 }
